90298359878b3fdf9d00324fa4e925c158824e6d
Sofonas <sofonas49@gmail.com>
2023-03-07 17:35:48 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+//oppgave1
+// var alfa= ['a','b','c','d','e','f']
+// console.log(alfa[0])
+
+// //oppgave2
+// var alfa= ['a','b','c','d','e','f']
+// var x = alfa.length -1
+// console.log(x);
+// console.log(alfa[x])
+
+//oppgave3
+// function alfa(liste) {
+//     let sum = 0;
+//     for (let i = 0; i < liste.length; i++) {
+//       sum += liste[i];
+//     }
+//     return sum;
+//   }
+// let liste = [3, 66, 45];
+// console.log(alfa(liste));
+
+// function alfa(liste) {
+//     let produkt = 1;
+//     for (let i = 0; i < liste.length; i++) {
+//       produkt *= liste[i];
+//     }
+//     return produkt;
+//   }
+//   let liste = [3, 66, 45];
+//   console.log(alfa(liste));
+
+//oppgave4
+// function alfa(liste1, liste2) {
+//     let sumListe = [];
+//     for (let i = 0; i < liste1.length; i++) {
+//       sumListe.push(liste1[i] + liste2[i]);
+//     }
+//     return sumListe;
+//   }
+// let liste1 = [1, 2, 3, 4, 5];
+// let liste2 = [0, 1, 2, 3, 4];
+// console.log(alfa(liste1, liste2));
